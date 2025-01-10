0b45a554688a930ab6207185c40aa975eb2ea566
Hans <hansvekoni@gmail.com>
2025-01-10 08:48:42 -0300

    refactor: contractV2 setData function

--- contracts/ContractV2.sol
+++ contracts/ContractV2.sol
@@ -4,21 +4,25 @@ import "./ContractV1.sol";
 
 contract ContractV2 is ContractV1 {
     // Evento para seguimiento de cambios en 'data'
-    event DataUpdated(string oldData, string data);
-    string public data;
+    event DataUpdated(string oldData, string newData);
+    uint public updateCount; // Contador de actualizaciones
 
     // contract logic v2
     // Función para actualizar data (practica override si anterior es virtual)
     function setData(string memory _data) public override {
-    string memory oldData = data;
-    data = _data;
+        require(bytes(_data).length > 0, "Data cannot be empty");
 
-    emit DataUpdated(oldData, _data);
-}
+        string memory oldData = getData(); //valor actual
+        super.setData(_data); // llamada a la superclase o implementacion original para actualizar
+        updateCount++; // Incrementar contador
+        emit DataUpdated(oldData, _data); // emitr evento d actualizacion
+    }
 
-function getNewData() public view returns (string memory) {
-    return string(abi.encodePacked(data, " (updated in V2)"));
-}
+    // número de actualizaciones
+    function getUpdateCount() public view returns (uint) {
+        return updateCount;
+    }
+
     //function suma
     function add(uint a, uint b) public pure returns (uint) {
         return a + b;
